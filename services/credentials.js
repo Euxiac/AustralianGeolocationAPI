0e50de4acd13710d9baf6b313aefb44c5263e227
Christie <christie.tanc@gmail.com>
2024-11-13 05:03:33 +0800

    removing creds

--- services/credentials.js
+++
@@ -1,7 +0,0 @@
-const OpenWeather_key = '828619469cc259300802f375e9106b7b';
-export const WorldTime_key = 'xl+bj9pdDdGTuVOhtYZ2nw==G26BSuZUCSXFMSjt';
-
-//export default is the one that comes out by default
-//export you need to specify
-
-export default OpenWeather_key;
