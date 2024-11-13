53a173085110ec17b831028aef4af62bf1c9e5cc
Christie <christie.tanc@gmail.com>
2024-11-14 03:09:59 +0800

    database endpoints

--- controllers/databaseController.js
+++ controllers/databaseController.js
@@ -13,3 +13,52 @@ export const updateCoords = async (req, res) => {
       res.status(500).json({message: `test ${error.message}`});
     }
   };
+
+  export const deleteCity = async (req, res) => {
+    try {
+      const country = req.params.country;
+        const state = req.params.state;
+        const city = req.params.city;
+      const data = await databaseService.postDeleteCity(country, state, city);
+      res.json({data});
+    } catch (error) {
+      res.status(500).json({message: `test ${error.message}`});
+    }
+  };
+
+  export const emptyTables = async (req, res) => {
+    try {
+      const data = await databaseService.postEmpty();
+      res.json({data});
+    } catch (error) {
+      res.status(500).json({message: `test ${error.message}`});
+    }
+  };
+
+  export const redrawTables = async (req, res) => {
+    try {
+      const data = await databaseService.postRedraw();
+      res.json({data});
+    } catch (error) {
+      res.status(500).json({message: `test ${error.message}`});
+    }
+  };
+
+  export const populateWithMockData = async (req, res) => {
+    try {
+      const data = await databaseService.postPopulateWithMockData();
+      res.json({data});
+    } catch (error) {
+      res.status(500).json({message: `test ${error.message}`});
+    }
+  };
+
+  
+  export const populate = async (req, res) => {
+    try {
+      const data = await databaseService.postPopulate();
+      res.json({data});
+    } catch (error) {
+      res.status(500).json({message: `test ${error.message}`});
+    }
+  };
